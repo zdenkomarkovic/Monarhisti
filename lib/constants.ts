c27c583f8c4b7508940b941d00e9293b2c9143c3
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-02-09 18:11:47 +0100

    .

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,16 +1,16 @@
 // Navigation Items
 export const NAVIGATION_ITEMS = [
-  { label: 'Vesti', href: '/vesti' },
-  { label: 'Saopštenja', href: '/saopstenja' },
-  { label: 'Kontakt', href: '/kontakt' },
-  { label: 'Predsedništvo', href: '/predsednistvo' },
-  { label: 'Finansijski izveštaji', href: '/finansijski-izvestaji' },
-  { label: 'Elektronske pristupnice', href: '/elektronske-pristupnice' },
+  { label: "Vesti", href: "/vesti" },
+  { label: "Saopštenja", href: "/saopstenja" },
+  { label: "Kontakt", href: "/kontakt" },
+  { label: "Predsedništvo", href: "/predsednistvo" },
+  { label: "Finansijski izveštaji", href: "/finansijski-izvestaji" },
+  { label: "Elektronske pristupnice", href: "/elektronske-pristupnice" },
 ];
 
 // About Section Content
 export const ABOUT_TEXT = {
-  title: 'Ko smo mi?',
+  title: "Ko smo mi?",
   content: `Srpski Pokret Monarhista je udruženje građana koje se zalaže za promenu ustavnog sistema u Srbiji iz Republike u Kraljevinu Srbiju. Zalažemo se za povratak dinastije Karađorđević u Srbiju.
 
 Srbiji su potrebne reforme: obrazovanja, zdravstva, privreде... Srbiji je mesto u Evropskoj uniji, i Srbija mora da se ugleda na skandinavske zemlje koje su u monarhiji: Dansku, Ujedinjene Kraljevske zemlje, Norvešku i Švedsku.
@@ -21,19 +21,19 @@ Naša vizija je moderna, demokratska, evropska Srbija sa ustavnom monarhijom koj
 // YouTube Videos
 export const YOUTUBE_VIDEOS = [
   {
-    id: '2p-7mY3_mSA',
-    title: 'Monarhija u Srbiji',
-    description: 'Video o monarhiji',
+    id: "2p-7mY3_mSA",
+    title: "Monarhija u Srbiji",
+    description: "Video o monarhiji",
   },
   {
-    id: 'zqUS8VT10Vs',
-    title: 'Monarhija u Srbiji - Deo 2',
-    description: 'Nastavak diskusije o monarhiji',
+    id: "zqUS8VT10Vs",
+    title: "Monarhija u Srbiji - Deo 2",
+    description: "Nastavak diskusije o monarhiji",
   },
   {
-    id: 'eTKLlpyRu4o',
-    title: 'Kratki video',
-    description: 'Kratki pregled',
+    id: "eTKLlpyRu4o",
+    title: "Kratki video",
+    description: "Kratki pregled",
   },
   // TODO: Dodati video tribine kada dobijete ID
   // { id: 'VIDEO_ID_TRIBINE' },
@@ -41,19 +41,19 @@ export const YOUTUBE_VIDEOS = [
 
 // Author Information
 export const AUTHOR = {
-  name: 'Vladimir Putnik',
-  nickname: 'Jauci sa Pastrika',
+  name: "Vladimir Putnik",
+  nickname: "Jauci sa Pastrika",
   year: 2025,
 };
 
 // Contact Information (Placeholders)
 export const CONTACT_INFO = {
-  email: 'kontakt@monarhisti.rs',
-  phone: '+381 XX XXX XXXX',
-  address: 'Beograd, Srbija',
+  email: "acokundacina032@gmail.com",
+  phone: "+381605443315",
+  address: "Beograd, Srbija",
   social: {
-    facebook: '#',
-    instagram: '#',
-    twitter: '#',
+    facebook: "#",
+    instagram: "#",
+    twitter: "#",
   },
 };
